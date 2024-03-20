d91fb784352abd9267b4bd70537bc6cdd4d5667a
Scott McCrindle <scott@mccrindle.me>
2024-03-20 10:06:19 -0400

    modified script

--- js/json.js
+++ js/json.js
@@ -12,10 +12,10 @@ async function populate() {
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
     const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    const iScream = await response;
-    console.log(iScream);
+    const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
-    
+        console.log(iScream);
+
     // STEP 9a: Invoke the populateHeader function here, then build it below
     
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
