21a1f4d653270aec767f3e18d4b7e559dfdd9e96
Scott McCrindle <scott@mccrindle.me>
2024-03-20 11:22:02 -0400

    Finished lesson code

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector("section");
 async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "https://smccrindle.github.io/comp1073module10/js/i-scream.json";
+    const requestURL = "https://smccrindle.github.io/comp1073lesson10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -18,7 +18,7 @@ async function populate() {
     // STEP 9a: Invoke the populateHeader function here, then build it below
     populateHeader(iScream);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+    showTopFlavors(iScream);
 }
 
 // STEP 3b: Call the populate() function
@@ -34,25 +34,36 @@ function populateHeader(jsonObj) {
     header.appendChild(headerH1);
 };
 /* STEP 10b: Assemble the showTopFlavors() function */
-function showTopFlavors() {
+function showTopFlavors(jsonObj) {
     // STEP 10c: Attache the JSON topFlavors object to a variable
-    //let topFlavors = jsonObj.topFlavors;
+    let topFlavors = jsonObj.topFlavors;
+    // console.log(topFlavors);
     // STEP 10d: Loop through the topFlavors object
     for (let i = 0; i < topFlavors.length; i ++) {
         // STEP 10e: build HTML elements for the content
-        
+        let article = document.createElement("article");
+        let h2 = document.createElement("h2");
+        let image = document.createElement("img");
+        let ul = document.createElement("ul");
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
-        
-
+        h2.textContent = topFlavors[i]["name"];
+        image.setAttribute("src", `images/${topFlavors[i]["image"]}`);
+        // console.log(image);
         // STEP 10g: Build a loop for the ingredients array in the JSON
-        
+        let ingredients = topFlavors[i]["ingredients"];
+        for (let j = 0; j < ingredients.length; j++) {
+            let listItem = document.createElement("li");
+            listItem.textContent = ingredients[j];
             // add the ingredient to the UL
-
+            ul.appendChild(listItem);
+        }
         // STEP 10h: Append each of the above HTML elements to the ARTICLE element
-        
+        article.appendChild(h2);
+        article.appendChild(image);
+        article.appendChild(ul);
         // STEP 10i: Append each complete ARTICLE element to the SECTION element
-        
+        section.appendChild(article);
     };
 };
 // STEP 11: The instructor will edit the JSON file - refresh your page to see the updated content
